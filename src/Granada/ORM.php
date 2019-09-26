d221af75661bd32e46db90efc8fd7ab6838853b0
Josh Marshall <josh@sanchia.com.au>
2019-09-27 09:15:34 +1000

    Fix docblock

--- src/Granada/ORM.php
+++ src/Granada/ORM.php
@@ -1179,7 +1179,7 @@ class ORM implements ArrayAccess {
      *    ->find_many();
      *
      * @param boolean $condition
-     * @param function $callback
+     * @param callable $callback
      * @return self
      */
     public function onlyif($condition, $callback) {
